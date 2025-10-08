983e2ead4fe79f3365b2ef5093caebc7cc62fd5b
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-08 13:58:19 +0200

    1-arrays n.3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -22,7 +22,10 @@ teachers[4] = "Patrick";{
 }
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();{
+  console.log(lastTeacher);
+  
+}
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
